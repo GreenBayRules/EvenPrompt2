04b6849240d596c79d290e799d6c6755fdc3f685
GreenBayRules <sheshank@outlook.com>
2019-04-12 18:27:26 -0700

    change config to not use environment variables

--- evenprompt2/config.py
+++ evenprompt2/config.py
@@ -2,12 +2,13 @@ import os
 
 class Config:
     
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = '6ea2e1c3b62394266c99f7ad6aab816c'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site1.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    # put real mail information later
+    MAIL_USERNAME = 'later@gmail.com'
+    MAIL_PASSWORD = 'later'
     
