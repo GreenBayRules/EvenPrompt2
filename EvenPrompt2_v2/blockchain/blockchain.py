d5dd240a3f114e11b2e6a63c646933bc4ccfeeb8
Dev <dev@Al-caphone-6.local>
2019-04-13 13:00:13 -0700

    fix

--- EvenPrompt2_v2/blockchain/blockchain.py
+++ EvenPrompt2_v2/blockchain/blockchain.py
@@ -28,10 +28,12 @@ from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 
 import hashlib
-import json
+import json`
 from time import time
-from urllib.parse import urlparse
-from uuid import uuid4
+try:
+    from urllib.parse import urlparse
+except ImportError:
+     from urlparse import urlparsefrom uuid import uuid4
 
 import requests
 from flask import Flask, jsonify, request, render_template
