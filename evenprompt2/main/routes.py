a791272e8d6ff88f103ef31104cccd4cc97faedf
GreenBayRules <sheshank@outlook.com>
2019-04-12 18:15:10 -0700

    quick changes

--- evenprompt2/main/routes.py
+++ evenprompt2/main/routes.py
@@ -1,12 +1,11 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, redirect, url_for
+from flask_login import current_user
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 @main.route("/home")
 def home():
+    if current_user.is_authenticated:
+        return redirect(url_for('users.portal'))
     return render_template("home.html", title="Welcome", subtitle="fintech")
-
-@main.route("/portal")
-def portal():
-    return render_template("portal.html", title="Portal", subtitle="View your credit score here")
